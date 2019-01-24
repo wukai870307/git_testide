841dfca4371023c006c2932a86cca9d60aa889a0
wukai <wukai870307@163.com>
2019-01-24 19:23:01 +0800

    把本地功能添加到conmmit

---
+++ src/cn/itheima/gitClass/firstClass.java
@@ -0,0 +1,17 @@
+package cn.itheima.gitClass;
+
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+        System.out.println("hello git project !");
+    }
+}
